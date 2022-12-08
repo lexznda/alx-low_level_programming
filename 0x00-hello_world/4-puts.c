558397801a9aae37fa14c0906ccbf9525593239c
lexznda <lexy4gold@gmail.com>
2022-12-07 23:50:01 -0800

    Task 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  *
- * Return Always 0 (Success)
+ * Return:i Always 0 (Success)
  */
 int main(void)
 {
-	char output[] = "Programming is like building a multilingual puzzle";
+	char output[] = "\"Programming is like building a multilingual puzzle\"";
+
 	puts(output);
 	return (0);
 }
