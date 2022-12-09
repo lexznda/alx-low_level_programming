3b858ced26906772635ba6d354173ffea5ea02ea
lexznda <lexy4gold@gmail.com>
2022-12-09 04:09:27 -0800

    Tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int a;
+
+	for(a = 97; a > 123; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
