d7b2ab953f662643d01642bc211f1fa9c31454b8
lexznda <lexy4gold@gmail.com>
2022-12-09 04:59:07 -0800

    Tasks

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-        int a;
+	int a;
 
-        for (a = 97; a < 123; a++)
+	for (a = 97; a < 123; a++)
 		if (a != 101 && a != 113)
 			putchar(a);
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
 
