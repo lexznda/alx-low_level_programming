77028167a6af08c5d7a8b86b328ba21f69fd252e
lexznda <lexy4gold@gmail.com>
2022-12-09 04:42:16 -0800

    Tasks

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-        int a;
+	int a;
 
-        for (a = 97; a < 123; a++)
-                putchar(a);
+	for (a = 97; a < 123; a++)
+		putchar(a);
 	for (a = 65; a < 91; a++)
 		putchar(a);
-        putchar('\n');
-        return (0);
-}
+	putchar('\n');
+	return (0);
+i}
