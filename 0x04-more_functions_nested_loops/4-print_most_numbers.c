e50d669afde614a21d6727e40fbe38edf3fbe7cb
lexznda <lexy4gold@gmail.com>
2022-12-15 05:41:23 -0800

    Task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,13 @@
  * Return: Nothing
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 4 || i != 2)
+		if (i != 4 && i != 2)
 		{
 			_putchar(i + '0');
 		}
