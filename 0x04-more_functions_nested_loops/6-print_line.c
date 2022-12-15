2f75e7aecb354f00ccc4092dd0c1d4a10e60ff5d
lexznda <lexy4gold@gmail.com>
2022-12-15 06:24:49 -0800

    Task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - Draws a striaght line in the terminal
+ * @n: number of line character
+ * Return: Void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+}
