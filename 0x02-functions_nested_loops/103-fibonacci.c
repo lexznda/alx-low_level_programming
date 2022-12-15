43c68eabafdc6a59b44f4617a19878665076c07e
lexznda <lexy4gold@gmail.com>
2022-12-14 18:39:06 -0800

    Tasj

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	long long int sum = 0;
+	long long int first = 0, second = 1, even, i;
+
+	for (i = 0; i < 33; i++)
+	{
+		sum = first + second;
+		if (sum % 2 == 0)
+		{
+			even = even + sum;
+		}
+		first = second;
+		second = sum;
+	}
+}
