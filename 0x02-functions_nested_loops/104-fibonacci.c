ff91fe49a67507543f280a8b447da90efc877f06
lexznda <lexy4gold@gmail.com>
2022-12-14 18:55:13 -0800

    Tasj

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,19 +6,19 @@
 
 int main(void)
 {
-	long int sum = 0;
-	long int first = 0, second = 1, i;
+	unsigned long int sum = 0;
+	unsigned long int first = 0, second = 1, i;
 
 	for (i = 0; i < 98; i++)
 	{
 		sum = first + second;
 		if (i == 97)
 			break;
-		printf("%li, ", sum);
+		printf("%lu, ", sum);
 		first = second;
 		second = sum;
 
 	}
-	printf("%li\n", sum);
+	printf("%lu\n", sum);
 	return (0);
 }
