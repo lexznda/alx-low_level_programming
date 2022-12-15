5b4a9ae8cbf03d8e345aee6ef6d4f95c59732535
lexznda <lexy4gold@gmail.com>
2022-12-14 18:17:51 -0800

    Tasj

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 	long int sum = 0;
-	int first = 0, second = 1, i;
+	long int first = 0, second = 1, i;
 
-	printf("%d, ", first);
+	printf("%li, ", first);
 	for (i = 0; i <= 50; i++)
 	{
 		sum = first + second;
