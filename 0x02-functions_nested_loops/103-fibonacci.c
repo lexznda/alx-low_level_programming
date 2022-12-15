d730d6e82a84dafd5c0bc133a1a6dc1fbf79a179
lexznda <lexy4gold@gmail.com>
2022-12-14 18:45:42 -0800

    Tasj

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,9 +8,8 @@ int main(void)
 	long int sum = 0;
 	long int first = 0, second = 1, even, i;
 
-	for (i = 0; i < 15; i++)
+	for (i = 0; i < 7; i++)
 	{
-		sum = first + second;
 		if (sum % 2 == 0)
 		{
 			even = even + sum;
