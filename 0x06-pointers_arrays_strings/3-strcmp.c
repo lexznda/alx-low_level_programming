577e0071c383d9094e4f3036c9c5e7384ef3383f
lexznda <lexy4gold@gmail.com>
2022-12-21 14:20:27 -0800

    Task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	/* unsigned int status = 0; */
+	int status_num = 0;
 
 	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	if (*s1 == *s2)
-		return (0);
-	else if (*s1 > *s2)
-		return (15);
-	else
-		return (-15);
+	status_num = (*s1 == *s2) ?0: (*s1 > *s2) ?15: -15;
+	return (status_num);
 }
