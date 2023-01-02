1486198e66bf9e8e582712e6c0ed9e5de09eccab
lexznda <lexy4gold@gmail.com>
2023-01-02 12:42:12 -0800

    three down

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: the string
+ * @c: the char
+ * Return: pointer to c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+
+	}
+	return (0);
+}
