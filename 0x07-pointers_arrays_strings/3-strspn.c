516859c357ea78dae0f16ff000a046e5c7139054
lexznda <lexy4gold@gmail.com>
2023-01-02 15:22:53 -0800

    fourth down

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: check
+ * @Return: unsigned int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, bool;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		bool = 1;
+
+		for (j = 0; accept[j] != 0; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
+}
