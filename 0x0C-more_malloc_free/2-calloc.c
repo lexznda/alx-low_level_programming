9e765d892bbf88d2ec92c1bd4b03a40f83139365
lexznda <lexy4gold@gmail.com>
2023-01-13 14:55:18 -0800

    Task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,10 +11,11 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	int *ptr;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 	{
