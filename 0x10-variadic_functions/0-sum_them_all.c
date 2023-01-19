6c3d97e4efbd8b3339b59d3a2c3ce2daa2f1ae60
lexznda <lexy4gold@gmail.com>
2023-01-19 06:49:28 -0800

    Task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,19 +1,19 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - sums all the parameters in 
+ * sum_them_all - sums all the parameters in
  * @n: the number of digits
  * Return: int
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
 	va_list ap;
 	unsigned int i;
 	int sum = 0;
 
+	if (n == 0)
+		return (0);
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
